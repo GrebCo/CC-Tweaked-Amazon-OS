1dff27c9fe325f38d5f69fc487ae583fe8105f54
efraim132 <efraim.egrebnev.grebnev605@gmail.com>
2025-05-19 18:56:08 -0700

    Update startup.lua

--- Client/startup.lua
+++ Client/startup.lua
@@ -1,1 +1,43 @@
+local localVersion = fs.open("localVersion.txt", "r")
+if localVersion then
+    local version = localVersion.readAll()
+    localVersion.close()
+else
+    local version = 0
+end
+
+shell.run("wget https://raw.githubusercontent.com/Easease/CC-Tweaked-Amazon-OS/refs/heads/main/version.txt remoteversion.txt")
+
+local file = fs.open("remoteversion.txt", "r")
+local remoteVersion = file.readAll()
+file.close()
+
+shell.run("delete remoteversion.txt")
+
+if remoteVersion ~= version then
+    print("A new version is available! Updating now.")
+    sleep(1)
+    print("Deleting old files...")
+    shell.run("delete localVersion.txt")
+    
+    shell.run("delete ClientNetworkHandler.lua")
+    shell.run("delete UI.lua")
+    shell.run("delete MiniMark.lua")
+    shell.run("delete browser.lua")
+    shell.run("delete network_config.lua")
+
+
+    print("Downloading new files...")
+    shell.run("wget https://raw.githubusercontent.com/Easease/CC-Tweaked-Amazon-OS/refs/heads/main/Client/ClientNetworkHandler.lua ClientNetworkHandler.lua")
+    shell.run("wget https://raw.githubusercontent.com/Easease/CC-Tweaked-Amazon-OS/refs/heads/main/Client/UI.lua UI.lua")
+    shell.run("wget https://raw.githubusercontent.com/Easease/CC-Tweaked-Amazon-OS/refs/heads/main/Client/MiniMark.lua MiniMark.lua")
+    shell.run("wget https://raw.githubusercontent.com/Easease/CC-Tweaked-Amazon-OS/refs/heads/main/Client/browser.lua browser.lua")
+    shell.run("wget https://raw.githubusercontent.com/Easease/CC-Tweaked-Amazon-OS/refs/heads/main/Client/network_config.lua network_config.lua")
+    shell.run("wget https://raw.githubusercontent.com/Easease/CC-Tweaked-Amazon-OS/refs/heads/main/Client/Default.txt Default.txt")
+
+    local newVersion = fs.open("localVersion.txt", "w")
+    newVersion.write(remoteVersion)
+    newVersion.close()
+end
+
 shell.run("browser.lua")
