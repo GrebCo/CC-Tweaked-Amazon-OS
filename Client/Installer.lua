d676c83e25d5c2d354c92e76d90a745e98094501
Easease <Elliot.hartzog@live.com>
2025-05-22 17:53:01 -0700

    Update Installer.lua

--- Client/Installer.lua
+++ Client/Installer.lua
@@ -9,9 +9,9 @@ end
 
 print("Current version: " .. version)
 
-shell.run("wget https://raw.githubusercontent.com/GrebCo/CC-Tweaked-Amazon-OS/refs/heads/Version-2.0/Client/Applications/EEBrowser/version.txt remoteversion.txt")
+shell.run("wget https://raw.githubusercontent.com/GrebCo/CC-Tweaked-Amazon-OS/refs/heads/Version-2.0/Client/version.txt remoteVersion.txt")
 
-local file = fs.open("remoteversion.txt", "r")
+local file = fs.open("remoteVersion.txt", "r")
 local remoteVersion = file.readAll()
 file.close()
 
@@ -27,15 +27,17 @@ if remoteVersion ~= version then
     
 
     shell.run("delete OSUtil/")
+    shell.run("delete Installer.lua")
     shell.run("mkdir OSUtil")
     
     print("Downloading new files...")
-    shell.run("wget https://raw.githubusercontent.com/GrebCo/CC-Tweaked-Amazon-OS/refs/heads/Version-2.0/Client/OSUtil/UI OSUtil/UI.lua")
+    shell.run("wget https://raw.githubusercontent.com/GrebCo/CC-Tweaked-Amazon-OS/refs/heads/Version-2.0/Client/OSUtil/UI.lua OSUtil/UI.lua")
     shell.run("wget https://raw.githubusercontent.com/GrebCo/CC-Tweaked-Amazon-OS/refs/heads/Version-2.0/Client/OSUtil/ClientNetworkHandler.lua OSUtil/ClientNetworkHandler.lua")
     shell.run("wget https://raw.githubusercontent.com/GrebCo/CC-Tweaked-Amazon-OS/refs/heads/Version-2.0/Client/OSUtil/MiniMark.lua OSUtil/MiniMark.lua")
+    shell.run("wget https://raw.githubusercontent.com/GrebCo/CC-Tweaked-Amazon-OS/refs/heads/Version-2.0/Client/Installer.lua Installer.lua")
     
     
-    local newVersion = fs.open("version.txt", "w")
+    local newVersion = fs.open("Version.txt", "w")
     newVersion.write(remoteVersion)
     newVersion.close()
 end
