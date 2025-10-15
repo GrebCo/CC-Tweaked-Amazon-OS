37d31b16a44ba9da35b44dd015f27c272e25c0f1
efraim132 <efraim.egrebnev.grebnev605@gmail.com>
2025-10-15 13:21:11 +0200

    updated fizzleTester.lua to print out new script format

--- Client/Applications/EEBrowser/fizzleTester.lua
+++ Client/Applications/EEBrowser/fizzleTester.lua
@@ -31,14 +31,28 @@ else
     log("Warning: minimark.getScripts() not found")
 end
 
+print("Loaded " .. tostring(#contextTable.scripts) .. " scripts from " .. FILE)
+
+
+-- for loop and print the scripts loaded
+log("Loaded scripts:")
+-- Simplify the scripts table into an array of {eventName, code} pairs
+for eventName, code in pairs(contextTable.scripts) do
+    print("Event: " .. eventName)
+    for _,line in pairs(code) do
+        print("  " .. line)
+    end
+end
+
+
 -- Initialize Fizzle with this context
-fizzle.init(contextTable)
-log("Fizzle initialized with context")
-
-fizzle.renew()
-log("Fizzle renewed")
-
-fizzle.triggerFizzleEvent("helloevent")
-log("Triggered 'helloevent'")
+-- fizzle.init(contextTable)
+-- log("Fizzle initialized with context")
+--
+-- fizzle.renew()
+-- log("Fizzle renewed")
+--
+-- fizzle.triggerFizzleEvent("helloevent")
+-- log("Triggered 'helloevent'")
 
 
