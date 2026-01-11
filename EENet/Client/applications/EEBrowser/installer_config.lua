9ebd7e1ace1e9a3a6aaaae0ba338e2584fc1f4f0
Easease <Elliot.hartzog@live.com>
2026-01-10 21:07:51 -0800

    Updated installer

---
+++ EENet/Client/applications/EEBrowser/installer_config.lua
@@ -0,0 +1,12 @@
+-- Configuration for Installer and Updater
+-- Easily change which branch and repository to pull from
+
+config = {
+    -- GitHub repository information
+    owner = "GrebCo",
+    repo = "CC-Tweaked-Amazon-OS",
+    branch = "EEnetv2",  -- Change this to switch branches
+
+    -- The base path in the repository (where EENet/Client is located)
+    basePath = "EENet/Client",
+}
